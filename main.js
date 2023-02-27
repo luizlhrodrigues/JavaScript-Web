d8f172448c208eb1f90aa48725cc0bb227acb800
Luiz Henrique Rodrigues <luiz.lhrodrigues@gmail.com>
2023-02-27 11:35:06 -0300

    finalizando

--- main.js
+++ main.js
@@ -15,4 +15,11 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
         tocaSom(idAudio);
     }
 
+    tecla.onkeydown = function () {
+        tecla.classList.add('ativa');
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.add('');
+    }
 }
